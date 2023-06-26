e1d93819e839bf5fbd8e977d3d51028062a864de
huongtxdo <huong.tx.do@gmail.com>
2023-06-26 13:40:39 +0300

    add test files

---
+++ backend/utils/for_testing.js
@@ -0,0 +1,11 @@
+const reverse = (string) => {
+  return string.split('').reverse().join('')
+}
+
+const average = (array) => {
+  return array.length === 0
+    ? 0
+    : array.reduce((sum, item) => sum + item, 0) / array.length
+}
+
+module.exports = { reverse, average }
