39e70842a100feaa0632cca12a95964e465add5a
Huong Laptop <huong.tx.do@gmail.com>
2023-07-20 11:56:52 +0300

    whole part 5c

--- frontend/src/components/NoteForm.test.js
+++ frontend/src/components/NoteForm.test.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import NoteForm from './NoteForm'
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import NoteForm from "./NoteForm";
 
-test('<NoteForm /> updates parent state and calls onSubmit', async () => {
-  const createNote = jest.fn()
-  const user = userEvent.setup()
+test("<NoteForm /> updates parent state and calls onSubmit", async () => {
+  const createNote = jest.fn();
+  const user = userEvent.setup();
 
-  render(<NoteForm createNote={createNote} />)
+  render(<NoteForm createNote={createNote} />);
 
-  const input = screen.getByRole('textbox')
-  const sendButton = screen.getByText('save')
+  const input = screen.getByRole("textbox");
+  // if there are more than 1 'textbox', use getAllByRole
+  // if there is placeholder, use getByPlaceholderText
+  // if there is id='note-input', use container.querySelector('#note-input')
+  const sendButton = screen.getByText("save");
 
-  await user.type(input, 'testing a form...')
-  await user.click(sendButton)
+  await user.type(input, "testing a form...");
+  // if there're more than 1 'textbox', use user.type(inputs[0])
+  await user.click(sendButton);
 
-  expect(createNote.mock.calls).toHaveLength(1)
-  expect(createNote.mock.calls[0][0].content).toBe('testing a form...')
-})
+  expect(createNote.mock.calls).toHaveLength(1);
+  expect(createNote.mock.calls[0][0].content).toBe("testing a form...");
+});
